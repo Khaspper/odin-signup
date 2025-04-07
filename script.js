52dd1e39e28ddd811ddae24099af7024e079ec33
Mark <brandonnarciso24@gmail.com>
2025-04-06 21:55:02 -0700

    Finish custom JS form validation
    passwords, phone number and email are now verified before sending forms

--- script.js
+++ script.js
@@ -2,27 +2,26 @@ const form = document.querySelector('form');
 const inputs = document.querySelectorAll('input');
 const errorMessage = document.querySelectorAll('span');
 
-// for (let i = 0; i < inputs.length; i++) {
-//   console.log(inputs[i].validity);
-// }
-
 form.addEventListener('submit', (event) => {
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].validity.valid) {
       errorMessage[i].textContent = "";
-      //! Remove active error class
-      errorMessage.className = "error";
+    }
+    else if (inputs[i].id === 'password-2' && !inputs[i].validity.valueMissing) {
+      let match = checkPassword(inputs[i], errorMessage[i]);
+      if (match === false) {
+        event.preventDefault();
+      }
     }
     else {
       showError(inputs[i], errorMessage[i]);
+      event.preventDefault();
     }
   }
-  event.preventDefault();
 })
 
 function showError(input, span) {
   message = input.validationMessage;
-  // let message = "";
 
   if (input.type == 'email' && input.validity.typeMismatch) {
     message = "Missing an '@' in the email";
@@ -30,8 +29,37 @@ function showError(input, span) {
       message = "Missing the domain name after '@'";
     }
   }
-  else if (input.type == 'password') {
-    message = "";
+  if (input.type == 'tel' && !input.validity.valueMissing) {
+    message = "Please use this format: XXX-XXX-XXXX";
   }
+  else if (input.id === 'password-1') {
+    if (input.validity.tooShort) {
+      message = "Must be at least 8 characters long";
+    }
+    else if (input.validity.patternMismatch) {
+      if (!/[A-Z]/.test(input.value)) {
+        message = "Must at least have 1 uppercase letter";
+      }
+      else if (!/[0-9]/.test(input.value)) {
+        message = "Must at least have 1 digit";
+      }
+      else if (!/[a-z]/.test(input.value)) {
+        message = "Must at least have 1 lowercase letter";
+      }
+      else if (/\s/.test(input.value)) {
+        message = "Spaces are not allowed";
+      }
+    }
+  }
+
   span.textContent = message;
+}
+
+function checkPassword(input, span) {
+  const firstPass = document.getElementById('password-1'); 
+  if (!(input.value === firstPass.value)) {
+    span.textContent = "Passwords do not match";
+    return false;
+  }
+  return true;
 }
